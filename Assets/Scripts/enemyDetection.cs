5f21ef33783ad7603cbced1a2b9701e1cdb44bc7
Sebbe <ssnnn017@edu.karlstad.se>
2023-10-26 12:37:10 +0200

    Enemy AI

--- Assets/Scripts/enemyDetection.cs
+++ Assets/Scripts/enemyDetection.cs
@@ -1,18 +1,106 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class enemyDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent agent;
+    
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+//patroll
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+//attack
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+//states
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+
+    private void Awake()
     {
-        
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
-        
+        //Check sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesnt move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Attack code here
+            //
+            Debug.Log("Attack!");
+
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 }
---
+++ Assets/Scripts/noRotation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class noRotation : MonoBehaviour
+{
+    void LateUpdate ()
+    {
+        transform.rotation = Quaternion.identity;  
+    }
+}
