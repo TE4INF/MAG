a739efc8f991823a185404db2cfa747db750a957
Vincent-Johansson <146840705+Vincent-Johansson@users.noreply.github.com>
2023-12-11 08:47:17 +0100

    Startmeny update

--- Assets/Scripts/StartmenyScript.cs
+++ Assets/Scripts/StartmenyScript.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class StartmenyScript : MonoBehaviour
 {
+    public GameObject X1;
+    public GameObject X2;
+    public GameObject Audio1;
+
+
     // Start is called before the first frame update
     public void Play()
     {
@@ -17,5 +22,31 @@ public class StartmenyScript : MonoBehaviour
     {
        Debug.Log("Quit");
        Application.Quit();
+    }
+
+    public void trigger1()
+    {
+      if(X1.activeInHierarchy == false)
+      {
+        X1.SetActive(true);
+        Audio1.SetActive(false);
+
+      }
+      else{
+        X1.SetActive(false);
+        Audio1.SetActive(true); 
+      }
+    }
+
+    public void trigger2()
+    {
+        if(X2.activeInHierarchy == false)
+        {
+            X2.SetActive(true);
+        }
+        else{
+            X2.SetActive(false);
+        }
+
     }
 }
