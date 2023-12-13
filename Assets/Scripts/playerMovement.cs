5f539e32a3d86c3ac8c38e56523365dda355be29
yoxenl <caisaskog@gmail.com>
2023-12-13 10:44:17 +0100

    flips the character when moving

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -9,6 +9,7 @@ public class playerMovement : MonoBehaviour
     public Rigidbody playerRb;
     public float walkSpeed, runSpeed, jumpForce, dodgeSpeed;
     private bool raycast;
+    public SpriteRenderer playerSprite;
 
     public LayerMask groundLayer;
     public Transform groundPoint;
@@ -16,7 +17,8 @@ public class playerMovement : MonoBehaviour
 
     private Vector2 moveInput;
 
-
+    bool isFacingRight = true;
+     
     void Update()
     {
         moveInput.x = Input.GetAxis("Horizontal");
@@ -24,7 +26,7 @@ public class playerMovement : MonoBehaviour
 
         moveInput.Normalize(); // Makes movement more smooth
 
-
+        // Moves left or right either walk or sprinting
         if (Input.GetKey(KeyCode.LeftShift))
         {
             playerRb.velocity = new Vector3(moveInput.x * runSpeed, playerRb.velocity.y, moveInput.y * runSpeed);
@@ -32,6 +34,15 @@ public class playerMovement : MonoBehaviour
         {
             playerRb.velocity = new Vector3(moveInput.x * walkSpeed, playerRb.velocity.y, moveInput.y * walkSpeed);
         }
+
+        if(moveInput.x < 0 && isFacingRight)
+        {
+            flip();
+        }
+        else if(moveInput.x > 0 && !isFacingRight) {
+            flip();
+        }
+
 
         RaycastHit hit;
         if (raycast = Physics.Raycast(groundPoint.position, Vector3.down, out hit, .01f, groundLayer))
@@ -47,6 +58,24 @@ public class playerMovement : MonoBehaviour
             playerRb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
 
         }
+    }
+
+    void flip()
+    {
+        if (isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            playerSprite.flipX = true;
+        }
+           
+        else
+        {
+            isFacingRight = true;
+            playerSprite.flipX = false;
+        }
+        
+
+
     }
 
 }
