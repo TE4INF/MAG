b336cbab2a6a8ebef476d39bc207a86758ec917c
AlfredMakinen <144239690+AlfredMakinen@users.noreply.github.com>
2024-01-31 14:01:24 +0100

    Lägger till början på en wave spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class Wave
+{
+    public string waveName;
+    public int noOfEnemies;
+    public GameObject[] typeOfEnemies;
+    public float spawnInterval;
+
+}
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+}
