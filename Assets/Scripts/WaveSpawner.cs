508192f736259d073441cdd3a1640ace1b62a1ed
AlfredMakinen <144239690+AlfredMakinen@users.noreply.github.com>
2024-02-01 14:17:43 +0100

    Fixar UI till waves

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 [System.Serializable]
 
 public class Wave
@@ -16,23 +19,33 @@ public class WaveSpawner : MonoBehaviour
 {
     public Wave[] waves;
     public Transform[] spawnPoints;
+    public Animator animator;
+    public TMP_Text waveName;
 
     private Wave currentWave;
     private int currentWaveNumber;
     private float nextSpawnTime;
 
     private bool canSpawn = true;
+    private bool canAnimate = false;
 
     private void Update()
     {
         currentWave = waves[currentWaveNumber];
         SpawnWave();
-        GameObject[] totalEnemies= GameObject.FindGameObjectsWithTag("Enemy");
-        if(totalEnemies.Length == 0 && !canSpawn && currentWaveNumber+1 !=waves.Length )
+        GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if(totalEnemies.Length == 0 && !canSpawn && currentWaveNumber+1 != waves.Length && canAnimate)
         {
-            currentWaveNumber++;
-            canSpawn = true;
+            waveName.text = waves[currentWaveNumber + 1].waveName;
+            animator.SetTrigger("WaveComplete");
+            canAnimate = false;
         }
+    }
+
+    void SpawnNextWave()
+    {
+        currentWaveNumber++;
+        canSpawn = true;
     }
 
     void SpawnWave()
@@ -47,6 +60,7 @@ public class WaveSpawner : MonoBehaviour
             if (currentWave.noOfEnemies == 0)
             {
                 canSpawn=false;
+                canAnimate = true;
             }
         }
         
