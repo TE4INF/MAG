fae7bc1e2b32223eca7067667265290dc65d7266
Sebbe <ssnnn017@edu.karlstad.se>
2024-02-05 14:03:46 +0100

    enemy prefab

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -34,7 +34,7 @@ public class WaveSpawner : MonoBehaviour
         currentWave = waves[currentWaveNumber];
         SpawnWave();
         GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
-        if(totalEnemies.Length == 0 && !canSpawn && currentWaveNumber+1 != waves.Length && canAnimate)
+        if (totalEnemies.Length == 0 && !canSpawn && currentWaveNumber + 1 != waves.Length && canAnimate)
         {
             waveName.text = waves[currentWaveNumber + 1].waveName;
             animator.SetTrigger("WaveComplete");
@@ -52,17 +52,17 @@ public class WaveSpawner : MonoBehaviour
     {
         if (canSpawn && nextSpawnTime < Time.time)
         {
-            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0,currentWave.typeOfEnemies.Length)];
-            Transform randomPoint = spawnPoints[Random.Range(0,spawnPoints.Length)];
+            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+            Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
             Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
             currentWave.noOfEnemies--;
             nextSpawnTime = Time.time + currentWave.spawnInterval;
             if (currentWave.noOfEnemies == 0)
             {
-                canSpawn=false;
+                canSpawn = false;
                 canAnimate = true;
             }
         }
-        
+
     }
 }
