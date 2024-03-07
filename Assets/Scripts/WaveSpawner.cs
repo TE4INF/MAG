f70ec388b448c683ed4546f6438a01e44aa1ef11
Vincent-Johansson <146840705+Vincent-Johansson@users.noreply.github.com>
2024-03-07 10:47:20 +0100

    fixar utseende

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -52,6 +52,7 @@ public class WaveSpawner : MonoBehaviour
     {
         if (canSpawn && nextSpawnTime < Time.time)
         {
+            Debug.Log("spawn incoming");
             GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
             Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
             Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
