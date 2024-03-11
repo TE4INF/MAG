d82eec6d8203de819fc1af2168471446f9c59f3b
Sebbe <ssnnn017@edu.karlstad.se>
2024-03-11 14:27:07 +0100

    attackIndiactor

---
+++ Assets/Scripts/AttackIndicator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackIndicator : MonoBehaviour
+{
+    public Vector2 Pointerposition { get; set; }
+    public Vector3 screenPos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenPos = Input.mousePosition;
+        screenPos.z = Camera.main.nearClipPlane + 1;
+        Pointerposition = screenPos;
+        transform.right = (Pointerposition - (Vector2)transform.localPosition).normalized;
+    }
+
+
+}
